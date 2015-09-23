2a90e2a3ae8e8669653727e28887941299770e02
Ramon Long <ramonjlongiii@gmail.com>
2015-09-23 18:22:40 -0400

    Adding Agreement table.

---
+++ ArcanaErp.Database/dbo/Agreement.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Agreement]
+(
+	[Id] uniqueidentifier CONSTRAINT Guid_Default DEFAULT NEWSEQUENTIALID() ROWGUIDCOL,
+    [Description] NVARCHAR(MAX),
+    [AgreementTypeId] UNIQUEIDENTIFIER NULL, 
+    [AgreementStatus] NVARCHAR(MAX) NULL, 
+    [ProductId] UNIQUEIDENTIFIER NULL, 
+    [AgreementDate] DATETIME NULL, 
+    [FromDate] DATETIME NULL, 
+    [ThroughDate] DATETIME NULL, 
+    [ExternalIdentifier] NVARCHAR(MAX) NULL, 
+    [ExternalIdentifierSource] NVARCHAR(MAX) NULL, 
+    CONSTRAINT Guid_PK PRIMARY KEY (Id)
+)
