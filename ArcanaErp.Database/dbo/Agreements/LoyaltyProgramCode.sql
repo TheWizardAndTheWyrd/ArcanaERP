29873fc61264d11bf2f3232c5fbc3c909971b07a
Ramon Long <ramonjlongiii@gmail.com>
2015-09-23 20:15:21 -0400

    Completing first iteration of Agreement schema definition.

---
+++ ArcanaErp.Database/dbo/Agreements/LoyaltyProgramCode.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LoyaltyProgramCode]
+(
+	[Id] uniqueidentifier CONSTRAINT LoyaltyProgramCodeGuidDefault DEFAULT NEWSEQUENTIALID() ROWGUIDCOL NOT NULL, 
+  
+    [Identifier] NVARCHAR(MAX) NULL, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    CONSTRAINT LoyaltyProgramCodeGuid_PK PRIMARY KEY (Id)
+)
